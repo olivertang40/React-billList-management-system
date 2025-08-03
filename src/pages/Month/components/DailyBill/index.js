9ebe0a5d220cc01f70cfcf8d4def3090605316b7
eric tang <ericjiantang@cnspartner.com>
2025-08-03 17:12:41 -0400

    dayBill list render and term mapping

--- src/pages/Month/components/DailyBill/index.js
+++ src/pages/Month/components/DailyBill/index.js
@@ -1,26 +1,27 @@
 import classNames from 'classnames'
 import './index.scss'
 import { useMemo } from 'react';
+import { billTypeToName } from "@/constants";
 
-const DailyBill = ({date, billList}) => {
-    const dayResult = useMemo(() => {
-      const pay = billList
-        .filter((item) => item.type === "pay")
-        .reduce((a, c) => a + c.money, 0);
-  
-      const income = billList
-        .filter((item) => item.type === "income")
-        .reduce((a, c) => a + c.money, 0);
-      console.log("计算 day result");
-      return { pay, income, total: pay + income };
-    }, [billList]);
+const DailyBill = ({ date, billList }) => {
+  const dayResult = useMemo(() => {
+    const pay = billList
+      .filter((item) => item.type === "pay")
+      .reduce((a, c) => a + c.money, 0);
+
+    const income = billList
+      .filter((item) => item.type === "income")
+      .reduce((a, c) => a + c.money, 0);
+    console.log("计算 day result");
+    return { pay, income, total: pay + income };
+  }, [billList]);
 
   return (
-    <div className={classNames('dailyBill')}>
+    <div className={classNames("dailyBill")}>
       <div className="header">
         <div className="dateIcon">
           <span className="date">{date}</span>
-          <span className={classNames('arrow')}></span>
+          <span className={classNames("arrow")}></span>
         </div>
         <div className="oneLineOverview">
           <div className="pay">
@@ -37,7 +38,22 @@ const DailyBill = ({date, billList}) => {
           </div>
         </div>
       </div>
+      {/* 单日列表 */}
+      <div className="billList">
+        {billList.map((item) => {
+          return (
+            <div className="bill" key={item.id}>
+              <div className="detail">
+                <div className="billType">{billTypeToName[item.useFor]}</div>
+              </div>
+              <div className={classNames("money", item.type)}>
+                {item.money.toFixed(2)}
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 export default DailyBill
