db719b1ebe5d9a0c02c2e39a091a5cc638b1f2b7
eric tang <ericjiantang@cnspartner.com>
2025-08-03 16:05:24 -0400

    statics data calculation based on chosen month

--- src/pages/Month/index.js
+++ src/pages/Month/index.js
@@ -20,10 +20,24 @@ const Month = () => {
     return dayjs(new Date()).format("YYYY-MM");
   });
 
+  const [currentMonthList, setMonthList] = useState([]);
+  const monthResult = useMemo(() => {
+    const pay = currentMonthList
+      .filter((item) => item.type === "pay")
+      .reduce((a, c) => a + c.money, 0);
+
+    const income = currentMonthList
+      .filter((item) => item.type === "income")
+      .reduce((a, c) => a + c.money, 0);
+
+    return { pay, income, total: pay + income };
+  }, [currentMonthList]);
+
   const onConfirm = (date) => {
     setDateVisible(false);
-    console.log(date);
     const formatDate = dayjs(date).format("YYYY-MM");
+    setMonthList(monthGroup[formatDate] || []);
+
     setCurrentDate(formatDate);
   };
 
@@ -50,15 +64,15 @@ const Month = () => {
           {/* 统计区域 */}
           <div className="twoLineOverview">
             <div className="item">
-              <span className="money">{100}</span>
+              <span className="money">{monthResult.pay.toFixed(2)}</span>
               <span className="type">支出</span>
             </div>
             <div className="item">
-              <span className="money">{200}</span>
+              <span className="money">{monthResult.income.toFixed(2)}</span>
               <span className="type">收入</span>
             </div>
             <div className="item">
-              <span className="money">{200}</span>
+              <span className="money">{monthResult.total.toFixed(2)}</span>
               <span className="type">结余</span>
             </div>
           </div>
